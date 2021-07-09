7fc00ab1f6def9a3492841728ca22ffac43a2a1d
Vittoria Thomasini <vittoriathomasini@hotmail.com>
2021-07-08 21:15:43 -0300

    Reformulation

--- ballon.py
+++ ballon.py
@@ -1,10 +1,11 @@
 ## 1-Create a function that says a given message inside a text "balloon"
 ## 2-Create a class named PythonSays which has a __init__ and a say(message) method
+## 2.1-Move the say_in_balloon to the say method
 class PythonSays():
     def __init__(self):
-            pass
-
-    def say_in_ballon(self,message):
+        pass
+        
+    def say(self, message):
         messages = message.split('\n')
         longest_line_size = (max([len(m) for m in messages]))
         linha = '  ' + '-' * longest_line_size
@@ -15,12 +16,13 @@ class PythonSays():
             print(f'/ {messages[0].center(longest_line_size)} \\')    
             for str_idx in range(len(messages) - 2):
                 print(f'| {messages[str_idx+1].center(longest_line_size)} |')
-
+            
             print(f'\\ {messages[-1].center(longest_line_size)} /')
         print(linha)
+        print(vader_ascii)
 
-say = PythonSays()     
-say.say_in_ballon('Hello there, General Kenobi')
-say.say_in_ballon('Hello there,\nGeneral Kenobi')
-say.say_in_ballon('Hello\nthere,\nGeneral Kenobi')
-say.say_in_ballon('Hello\nthere,\nGeneral\nKenobi')
+ps = PythonSays()
+ps.say('Hello there, General Kenobi')
+ps.say('Hello there,\nGeneral Kenobi')
+ps.say('Hello\nthere,\nGeneral Kenobi')
+ps.say('Hello\nthere,\nGeneral\nKenobi')
